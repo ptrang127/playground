f006132698ce073e1d6e7bf98f6e7a099dfa1adf
ptrang127 <p.trang127@gmail.com>
2017-01-31 18:12:43 -0700

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	
-	cout << "Hello, world!" << endl;
-
+int main()
+{
+    cout << "Hello World!" << endl;;
+    return 0;
 }
